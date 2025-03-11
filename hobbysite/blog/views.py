b8cb1e92da0249fa1779e9f3fe19dca8e7424db9
mcfc <gregoriodelfinpascua@gmail.com>
2025-03-11 22:48:57 +0800

    Fixed typo (def to class) in views.py

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -2,10 +2,10 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Article
 
-def ArticleListView(ListView):
+class ArticleListView(ListView):
     model = Article
     template_name = 'blog_list.html'
 
-def ArticleDetailView(DetailView):
+class ArticleDetailView(DetailView):
     model = Article
     template_name = 'blog_article.html'
