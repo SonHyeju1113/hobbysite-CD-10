9b77864c63b4a8c6020a1d04b63573ba5234691f
mcfc <gregoriodelfinpascua@gmail.com>
2025-03-11 21:54:21 +0800

    Initialized Article model
    Note that created_on only gets set when the model is created, updated_on always updates on last model update, and the model is sorted by its date in descending order (class Meta)

--- hobbysite/blog/models.py
+++ hobbysite/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class ArticleCategory(models.Model):
     name = models.CharField(max_Length=255)
@@ -9,3 +10,19 @@ class ArticleCategory(models.Model):
     
     class Meta:
         ordering = ['name']
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(ArticleCategory, on_delete=models.SET_NULL, null=True, related_name="category")
+    entry = models.TextField(blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('article_detail', args=[str(self.name)])
+    
+    class Meta:
+        ordering = ['-created_on']
