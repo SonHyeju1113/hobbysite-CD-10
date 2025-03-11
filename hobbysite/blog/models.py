2d1f9010528cd1d8d72e4ca77070cd01560d500d
mcfc <gregoriodelfinpascua@gmail.com>
2025-03-11 21:36:05 +0800

    Initialized ArticleCategory model
    Note that ordering of the article category model is in alphabetical order with respect to its name (class Meta)

--- hobbysite/blog/models.py
+++ hobbysite/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class ArticleCategory(models.Model):
+    name = models.CharField(max_Length=255)
+    description = models.TextField(blank=False)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['name']
