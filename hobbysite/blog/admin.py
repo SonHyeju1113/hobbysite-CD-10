a65cdee548e8ce8e0afb57e8184353e1753b6d95
mcfc <gregoriodelfinpascua@gmail.com>
2025-03-11 22:30:15 +0800

    Changed InLine to ModelAdmin

--- hobbysite/blog/admin.py
+++ hobbysite/blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Article, ArticleCategory
 
-class ArticleAdmin(admin.TabularInline):
+class ArticleAdmin(admin.ModelAdmin):
     model = Article
 
 class ArticleCategoryAdmin(admin.ModelAdmin):
