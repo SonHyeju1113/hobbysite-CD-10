6056dbf7067e415ed72fae20ba35f5d13b705542
mcfc <gregoriodelfinpascua@gmail.com>
2025-03-11 21:26:45 +0800

    Initialized temporary views for blog list and articles

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+def blog_list(request):
+    return HttpResponse("You are at Blog Lists")
+
+def blog_article(request):
+    return HttpResponse("You are at Blog Article")
+
