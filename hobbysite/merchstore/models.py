5d4c4e888474f2241acd743b8345ae04cc1640a4
JavaDucks <jedale.sabio@student.ateneo.edu>
2025-03-13 14:48:03 +0800

    added new class Product Type

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class ProductType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=False)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = "Product Types"
