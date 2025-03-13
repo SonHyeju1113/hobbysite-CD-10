925e629950a426df112e87dc1abf213308574511
Juan <dad3ztroyah@gmail.com>
2025-03-13 15:04:09 +0800

    Implemented models for the wiki app

--- hobbysite/wiki/models.py
+++ hobbysite/wiki/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class ArticleCategory(models.Model):
+    name = models.CharField(max_length=255)
+    
+    description = models.TextField
+
+    class Meta:
+        ordering = ['name']
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+
+    category = models.ForeignKey(ArticleCategory, on_delete=models.SET_NULL, related_name='article')
+
+    entry = models.TextField
+
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_on']
