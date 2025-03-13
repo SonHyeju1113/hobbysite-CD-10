d86758dacda92d4a5583c69577f6b0d7a80db3cd
mcfc <gregoriodelfinpascua@gmail.com>
2025-03-14 00:10:54 +0800

    Removed trailing whitespaces in models.py

--- hobbysite/blog/models.py
+++ hobbysite/blog/models.py
@@ -7,22 +7,25 @@ class ArticleCategory(models.Model):
 
     def __str__(self):
         return self.name
-    
+
     class Meta:
         ordering = ['name']
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
-    category = models.ForeignKey(ArticleCategory, on_delete=models.SET_NULL, null=True, related_name="category")
+    category = models.ForeignKey(ArticleCategory,
+                                 on_delete=models.SET_NULL,
+                                 null=True,
+                                 related_name="category")
     entry = models.TextField(blank=False)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
-    
+
     def get_absolute_url(self):
         return reverse('article_detail', args=[str(self.pk)])
-    
+
     class Meta:
         ordering = ['-created_on']
