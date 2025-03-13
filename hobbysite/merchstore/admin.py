9e4be0fb11bcd60a662be40230e62c25fe4e7212
JavaDucks <jedale.sabio@student.ateneo.edu>
2025-03-13 15:08:09 +0800

    added in admin new class admin ProductAdmin

--- hobbysite/merchstore/admin.py
+++ hobbysite/merchstore/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import ProductType
+from .models import ProductType, Product
 
 class ProductTypeAdmin(admin.ModelAdmin):
     model = ProductType
     list_display = ('name', 'description')
     ordering = ['name']
+
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
