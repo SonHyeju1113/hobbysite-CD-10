469a29b8eb66dea35c19bfeedc76efb69aac3b5e
JavaDucks <jedale.sabio@student.ateneo.edu>
2025-03-13 16:26:46 +0800

    remove whitespaces in parameters of ProductModel

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -17,7 +17,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     product_type = models.ForeignKey(ProductType, on_delete=models.SET_NULL, null=True, related_name = "product_type")
     description = models.TextField(blank=False)
-    price = models.DecimalField( decimal_places=2)
+    price = models.DecimalField(decimal_places=2)
     
     def __str__(self):
         return self.name
