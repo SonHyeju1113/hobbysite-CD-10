99df9daf097fafeb2fe65eb87ae949b91e551525
JavaDucks <jedale.sabio@student.ateneo.edu>
2025-03-13 14:50:50 +0800

    added new class in models, Product class

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -12,3 +12,18 @@ class ProductType(models.Model):
     class Meta:
         ordering = ['name']
         verbose_name_plural = "Product Types"
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    product_type = models.ForeignKey(ProductType, on_delete=models.SET_NULL, null=True, related_name = "product_type")
+    description = models.TextField(blank=False)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('merchstore:product_detail', args=[str(self.pk)])
+    
+    class Meta:
+        ordering = ['name']
