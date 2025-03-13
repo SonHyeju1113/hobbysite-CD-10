2860ed0c0d386d49cb426cc1e262f5ea60918b48
JavaDucks <jedale.sabio@student.ateneo.edu>
2025-03-13 15:22:11 +0800

    modified merchstore/items url patterns

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ProductListView, ProductDetailView
 
 urlpatterns = [
-    path('merchstore/items', ProductListView.as_view(), name='product_detail'),
+    path('merchstore/items', ProductListView.as_view(), name='product_list'),
     path('merchstore/item/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
 ]
 
