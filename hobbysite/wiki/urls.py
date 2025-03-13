9ae26ddd08f5c177e70a702fef56f1fe11d461b0
Juan <dad3ztroyah@gmail.com>
2025-03-13 22:58:37 +0800

    Implemented urls. Implemented base version of views.

---
+++ hobbysite/wiki/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import ArticleList, ArticleDetail
+
+urlpatterns = [
+    path('wiki/articles/', ArticleList.as_view(), name = 'article_list'),
+    path('wiki/article/', ArticleDetail.as_view(), name = 'article_detail')
+]
+
+app_name = 'wiki'
--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Article
 
 # Create your views here.
+class ArticleList(ListView):
+    model = Article
+
+class ArticleDetail(DetailView):
+    model = Article
