70e2d34578f001df16998aaf5de6cd1345e24378
Juan <dad3ztroyah@gmail.com>
2025-03-14 02:11:36 +0800

    Added filters to admin

--- hobbysite/wiki/admin.py
+++ hobbysite/wiki/admin.py
@@ -8,10 +8,18 @@ class ArticleInline(admin.TabularInline):
 class ArticleAdmin(admin.ModelAdmin):
     model = Article
 
+    search_fields = ('title', 'created_on', 'updated_on',)
+    list_display = ('title', 'created_on', 'updated_on',)
+    list_filter = ('title', 'created_on', 'updated_on',)
+
     fieldsets = [('Details', {'fields': ['title', 'entry']})]
 
 class ArticleCategoryAdmin(admin.ModelAdmin):
     inlines = [ArticleInline,]
+
+    search_fields = ('name',)
+    list_display = ('name',)
+    list_filter = ('name',)
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(ArticleCategory, ArticleCategoryAdmin)
