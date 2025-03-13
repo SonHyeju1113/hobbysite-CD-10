c2691344577f29a589ada4ec2cf20638473c34a1
JavaDucks <jedale.sabio@student.ateneo.edu>
2025-03-13 14:56:04 +0800

    added in app views the ProductListView class

--- hobbysite/merchstore/views.py
+++ hobbysite/merchstore/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Product
 
-# Create your views here.
+class ProductListView(ListView):
+    model = Product
+    template_name = 'product_list.html'
