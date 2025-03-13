b5121a540f2c00c24948d3c95dfe889f62c21fa1
mcfc <gregoriodelfinpascua@gmail.com>
2025-03-13 23:48:21 +0800

    Added "/" at the end of url paths in urls.py
    To prevent possible errors

--- hobbysite/blog/urls.py
+++ hobbysite/blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ArticleListView, ArticleDetailView
 
 urlpatterns = [
-    path('blog/articles', ArticleListView.as_view(), name='article_detail'),
-    path('blog/article/<int:pk>', ArticleDetailView.as_view(), name='article_detail'),
+    path('blog/articles/', ArticleListView.as_view(), name='article_detail'),
+    path('blog/article/<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
 ]
