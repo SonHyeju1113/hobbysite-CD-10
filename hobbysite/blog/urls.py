48a71aca20d09a4f787ae2c9350bf86bf56e8dcb
mcfc <gregoriodelfinpascua@gmail.com>
2025-03-14 14:03:18 +0800

    Renamed name of articles path to articles
    Remove duplicate name

--- hobbysite/blog/urls.py
+++ hobbysite/blog/urls.py
@@ -5,6 +5,6 @@ from django.urls import path
 from .views import ArticleListView, ArticleDetailView
 
 urlpatterns = [
-    path('blog/articles/', ArticleListView.as_view(), name='article_detail'),
+    path('blog/articles/', ArticleListView.as_view(), name='articles'),
     path('blog/article/<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
 ]
