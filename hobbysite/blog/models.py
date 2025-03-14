72f5ea6104ecb7eb44bbc09aa6ca7149da15877c
mcfc <161129995+mcfcs@users.noreply.github.com>
2025-03-14 08:04:13 +0800

    Added class and method docstrings to models.py

--- hobbysite/blog/models.py
+++ hobbysite/blog/models.py
@@ -1,7 +1,13 @@
+"""
+@brief Models for Blog app.
+"""
 from django.db import models
 from django.urls import reverse
 
 class ArticleCategory(models.Model):
+    """
+    @brief Instantiates ArticleCategory model.
+    """
     name = models.CharField(max_length=255)
     description = models.TextField(blank=False)
 
@@ -9,9 +15,15 @@ class ArticleCategory(models.Model):
         return self.name
 
     class Meta:
+        """
+        @brief To sort the model in ascending order with respect to model name.
+        """
         ordering = ['name']
 
 class Article(models.Model):
+    """
+    @brief Instantiates Article model.
+    """
     title = models.CharField(max_length=255)
     category = models.ForeignKey(ArticleCategory,
                                  on_delete=models.SET_NULL,
@@ -25,7 +37,14 @@ class Article(models.Model):
         return self.title
 
     def get_absolute_url(self):
+        """
+        @brief returns the specific URL for viewing the current 
+        instance with primary key as argument.
+        """
         return reverse('article_detail', args=[str(self.pk)])
 
     class Meta:
+        """
+        @brief To sort the model in descending order with respect to date created.
+        """
         ordering = ['-created_on']
