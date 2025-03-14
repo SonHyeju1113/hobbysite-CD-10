752a735c742a34611de8e109e9420ce53284ce76
boyadik69 <4Zhiraphael@gmail.com>
2025-03-14 09:17:49 +0800

    Added slashes to the end of the urls in forum urls.py

--- hobbysite/forum/urls.py
+++ hobbysite/forum/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import PostListView, PostDetailView
 
 urlpatterns = [
-    path('forum/threads', PostListView.as_view(), name = 'post_detail'), 
-    path('forum/thread/<int:pk>', PostDetailView.as_view(), name = 'post_detail'),
+    path('forum/threads/', PostListView.as_view(), name = 'post_detail'), 
+    path('forum/thread/<int:pk>/', PostDetailView.as_view(), name = 'post_detail'),
 ]
 
 app_name = "forum"
