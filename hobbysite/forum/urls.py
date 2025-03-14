9afadb585a0905aa3fa332d69bd0c8cfea69fb6b
boyadik69 <4Zhiraphael@gmail.com>
2025-03-14 10:09:15 +0800

    changed name parameter of threads url in urls.py

--- hobbysite/forum/urls.py
+++ hobbysite/forum/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import PostListView, PostDetailView
 
 urlpatterns = [
-    path('forum/threads/', PostListView.as_view(), name = 'post_detail'), 
+    path('forum/threads/', PostListView.as_view(), name = 'post_list'), 
     path('forum/thread/<int:pk>/', PostDetailView.as_view(), name = 'post_detail'),
 ]
 
