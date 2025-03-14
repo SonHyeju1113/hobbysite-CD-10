76216e0d69b05060df3356b60debe1485a17c144
Giscash0 <jmgpiscasio@gmail.com>
2025-03-14 15:09:49 +0800

    added CommentAdmin

--- hobbysite/commissions/admin.py
+++ hobbysite/commissions/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Commission
+from .models import Commission, Comment
 
 class CommissionAdmin(admin.ModelAdmin):
     model = Commission
+
+class CommentAdmin(admin.ModelAdmin):
+    model = Comment
