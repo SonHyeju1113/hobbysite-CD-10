695b5aa549e8ba552d52f9b9989e7fbd2e410495
mcfc <161129995+mcfcs@users.noreply.github.com>
2025-03-14 08:14:36 +0800

    Added class and module docstring to views.py

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -1,11 +1,20 @@
+"""
+@brief Class-Based Views for Blog app.
+"""
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Article, ArticleCategory
 
 class ArticleListView(ListView):
+    """
+    @brief List View class that uses the model ArticleCategory from models.py
+    """
     model = ArticleCategory
     template_name = 'blog_list.html'
 
 class ArticleDetailView(DetailView):
+    """
+    @brief Detail View clas that uses the model Article from models.py
+    """
     model = Article
     template_name = 'blog_article.html'
