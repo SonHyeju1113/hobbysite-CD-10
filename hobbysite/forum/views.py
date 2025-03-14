25112609097fb9da66b3fb5ad44d477447f14752
boyadik69 <4Zhiraphael@gmail.com>
2025-03-14 23:45:56 +0800

    added import for PostCategory in views.py, edited forum_list.html

--- hobbysite/forum/views.py
+++ hobbysite/forum/views.py
@@ -1,9 +1,9 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .models import Post
+from .models import Post, PostCategory
 
 class PostListView(ListView):
-    model = Post
+    model = PostCategory
     template_name = 'forum_list.html'
 
 class PostDetailView(DetailView):
