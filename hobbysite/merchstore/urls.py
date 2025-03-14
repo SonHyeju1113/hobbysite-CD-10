d1c5656c7fad4e898b02a6d012d6fed4fb2cefb3
JavaDucks <jedale.sabio@student.ateneo.edu>
2025-03-14 09:15:21 +0800

    added a / in app urls

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import ProductListView, ProductDetailView
 
 urlpatterns = [
-    path('merchstore/items', ProductListView.as_view(), name='product_list'),
-    path('merchstore/item/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
+    path('merchstore/items/', ProductListView.as_view(), name='product_list'),
+    path('merchstore/item/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
 ]
 
 app_name = "merchstore"
