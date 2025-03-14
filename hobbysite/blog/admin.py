1d8fe0e99f6aceaaf21fb9128383bf4225333b06
mcfc <161129995+mcfcs@users.noreply.github.com>
2025-03-14 08:19:18 +0800

    Added module and class docstrings to admin.py

--- hobbysite/blog/admin.py
+++ hobbysite/blog/admin.py
@@ -1,10 +1,19 @@
+"""
+@brief Admin module for Blog app.
+"""
 from django.contrib import admin
 from .models import Article, ArticleCategory
 
 class ArticleAdmin(admin.ModelAdmin):
+    """
+    @brief Instantiates Article model to ArticleAdmin class
+    """
     model = Article
 
 class ArticleCategoryAdmin(admin.ModelAdmin):
+    """
+    @brief Instantiates ArticleCategory model to ArticleCategoryAdmin class
+    """
     model = ArticleCategory
 
 admin.site.register(Article, ArticleAdmin)
