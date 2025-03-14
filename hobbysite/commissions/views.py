6dbbdcdee5ef8d77778afe8abc425270a947c8ac
Giscash0 <jmgpiscasio@gmail.com>
2025-03-14 14:28:38 +0800

    Added CommentListView class and initialized Comment as model

--- hobbysite/commissions/views.py
+++ hobbysite/commissions/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Comment
 
-# Create your views here.
+class CommentListView(ListView):
+    model = Comment
+    template_name = 'comment_list.html'
