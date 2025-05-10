e62d6433233eb59ec663b35efea9f1bf285bc0cf
mcfc <gregoriodelfinpascua@gmail.com>
2025-05-11 02:05:45 +0800

    Added path to create a new blog

--- hobbysite/blog/urls.py
+++ hobbysite/blog/urls.py
@@ -2,9 +2,10 @@
 @brief Url paths for Blog app.
 """
 from django.urls import path
-from .views import ArticleListView, ArticleDetailView
+from .views import ArticleListView, ArticleDetailView, ArticleCreateView
 
 urlpatterns = [
     path('blog/articles/', ArticleListView.as_view(), name='articles'),
     path('blog/article/<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
+    path('blog/create/', ArticleCreateView.as_view(), name='article_create'),
 ]
