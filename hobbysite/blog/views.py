cbaa18f95fd4a5e4bb425b39a83cd3e46ca892e5
mcfc <gregoriodelfinpascua@gmail.com>
2025-05-11 02:05:16 +0800

    Added ArticleCreateView in views.py

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -3,8 +3,11 @@
 """
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .models import Article, ArticleCategory
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
+from .models import Article, ArticleCategory
+from .forms import ArticleCreateForm
 
 class ArticleListView(LoginRequiredMixin,ListView):
     """
@@ -19,3 +22,9 @@ class ArticleDetailView(DetailView):
     """
     model = Article
     template_name = 'blog_article.html'
+
+class ArticleCreateView(CreateView):
+    model = Article
+    form_class = ArticleCreateForm
+    template_name = 'blog_create.html'
+    success_url = reverse_lazy('articles')
