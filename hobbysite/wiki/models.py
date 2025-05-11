fa01b2935baccf82f037213abd6af4ea8a946f94
Juan <dad3ztroyah@gmail.com>
2025-05-12 04:40:11 +0800

    Added developer exclusivitey for 'category' field in Article. Created Comment model

--- hobbysite/wiki/models.py
+++ hobbysite/wiki/models.py
@@ -16,9 +16,11 @@ class ArticleCategory(models.Model):
 class Article(models.Model):
     title = models.CharField(max_length=255)
 
-    category = models.ForeignKey(ArticleCategory, on_delete=models.SET_NULL, null=True, related_name='article')
+    category = models.ForeignKey(ArticleCategory, on_delete=models.SET_NULL, null=True, related_name='article', limit_choices_to={"is_staff": True},)
 
     entry = models.TextField(blank=False, default="")
+
+    header_image = models.ImageField(upload_to='images/', null=False, default='')
 
     created_on = models.DateTimeField(auto_now_add=True)
 
@@ -32,3 +34,17 @@ class Article(models.Model):
 
     class Meta:
         ordering = ['-created_on']
+
+class Comment(models.Model):
+    author = models.ForeignKey('user_management.Profile', on_delete=models.SET_NULL, null=True)
+
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+
+    entry = models.TextField(blank=False, default="")
+
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_on']
