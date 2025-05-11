7711adee32cf939079d85d659d5d7754986aa6f0
studentidle <jedale.sabio@student.ateneo.edu>
2025-05-11 19:12:26 +0800

    Update forms.py
    created class ProductForm

--- hobbysite/merchstore/forms.py
+++ hobbysite/merchstore/forms.py
@@ -5,3 +5,9 @@ class ProductTypeForm(forms.ModelForm):
     class Meta:
         model = ProductType
         fields = '__all__'
+    
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product 
+        fields = '__all__'
+        exclude = ['owner']
