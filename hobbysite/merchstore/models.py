be02fcebdea34e69f97a644c99980ff02543e6a9
studentidle <jedale.sabio@student.ateneo.edu>
2025-05-11 21:43:56 +0800

    Update models.py
    modified Product Model

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -26,18 +26,20 @@ class Product(models.Model):
     Instantiates Product Model and return name and absolute URL.
     """
     name = models.CharField(max_length=255)
-    product_type = models.ForeignKey(ProductType, on_delete=models.SET_NULL, null=True, related_name = "product_type")
-    description = models.TextField(blank=False)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    product_type = models.ForeignKey(ProductType,null=True ,on_delete=models.SET_NULL, related_name='products')
+    owner = models.ForeignKey('user_management.Profile',on_delete=models.CASCADE)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    STATUS_CHOICES = (
+        ('available', 'Available'),
+        ('on_sale', 'On Sale'),
+        ('out_of_stock', 'Out of Stock'),
+    )
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='available')
+
+    class Meta:
+        ordering = ['name']
 
     def __str__(self):
         return self.name
-
-    def get_absolute_url(self):
-        return reverse('merchstore:product_detail', args=[str(self.pk)])
-
-    class Meta:
-        """
-        To sort the model in ascending order with respect to model name.
-        """
-        ordering = ['name']
