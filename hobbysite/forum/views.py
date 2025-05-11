7fc7c9433f5a167f93081b952425d8c6120cd3b8
boyadik69 <4Zhiraphael@gmail.com>
2025-05-11 15:52:53 +0800

    added functionality to ThreadListView class and thread_list.html template

--- hobbysite/forum/views.py
+++ hobbysite/forum/views.py
@@ -6,6 +6,19 @@ class ThreadListView(ListView):
     model = ThreadCategory
     template_name = 'thread_list.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+
+        if user.is_authenticated:
+            context['my_threads'] = Thread.objects.filter(author = user)
+            context['other_threads'] = Thread.objects.exclude(author = user)
+        else:
+            context['my_threads'] = []
+            context['other_threads'] = Thread.objects.all()
+
+        return context
+
 class ThreadDetailView(DetailView):
     model = Thread
     template_name = 'thread_detail.html'
