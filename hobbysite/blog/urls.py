36da69e82028713fd8c2c92a11dcef92567263e7
mcfc <gregoriodelfinpascua@gmail.com>
2025-05-11 17:23:21 +0800

    Added ArticleUpdateView to urls

--- hobbysite/blog/urls.py
+++ hobbysite/blog/urls.py
@@ -2,10 +2,11 @@
 @brief Url paths for Blog app.
 """
 from django.urls import path
-from .views import ArticleListView, ArticleDetailView, ArticleCreateView
+from .views import ArticleListView, ArticleDetailView, ArticleCreateView, ArticleUpdateView
 
 urlpatterns = [
     path('blog/articles/', ArticleListView.as_view(), name='articles'),
     path('blog/article/<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
     path('blog/create/', ArticleCreateView.as_view(), name='article_create'),
+    path('blog/update/<int:pk>/', ArticleUpdateView.as_view(), name='article_update'),
 ]
