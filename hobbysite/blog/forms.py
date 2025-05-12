5e4f0d1463cfdd1f2e2f93234a8e55ffaf9de7ad
mcfc <gregoriodelfinpascua@gmail.com>
2025-05-12 20:37:09 +0800

    Added inlineformset to support multiple forms in one model (for gallery)

--- hobbysite/blog/forms.py
+++ hobbysite/blog/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Article, Comment
+from django.forms import inlineformset_factory
+from .models import Article, Comment, Gallery
 
 class ArticleCreateForm(forms.ModelForm):
     class Meta:
@@ -10,3 +11,10 @@ class ArticleCommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['entry']
+
+ArticleGalleryForm = inlineformset_factory(
+    Article, Gallery,
+    fields=['image', 'description'],
+    extra=1,
+    can_delete=True
+)
