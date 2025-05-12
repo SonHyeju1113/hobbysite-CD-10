afaea3452b163a54415a201510a0a0801b873ac5
Juan <dad3ztroyah@gmail.com>
2025-05-12 16:44:47 +0800

    Fixed url paths

--- hobbysite/wiki/urls.py
+++ hobbysite/wiki/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import ArticleList, ArticleDetail
 
 urlpatterns = [
-    path('wiki/articles/', ArticleList.as_view(), name = 'article_list'),
-    path('wiki/article/<int:pk>/', ArticleDetail.as_view(), name = 'article_detail')
+    path('articles/', ArticleList.as_view(), name = 'article_list'),
+    path('article/<int:pk>/', ArticleDetail.as_view(), name = 'article_detail')
 ]
 
 app_name = 'wiki'
