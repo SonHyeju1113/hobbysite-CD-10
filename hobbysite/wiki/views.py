84afb7a1dc2d385f5bea36cbdd1e4dfc73882b5c
Juan <dad3ztroyah@gmail.com>
2025-05-12 20:37:39 +0800

    Added author query for list view

--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -3,10 +3,22 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView
 from .models import Article
 
-# Create your views here.
 class ArticleList(ListView):
     model = Article
     template_name = 'wiki_list.html'
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return Article.objects.exclude(author=self.request.user)
+        return super().get_queryset()
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if self.request.user.is_authenticated:
+            user_articles = Article.objects.filter(author=self.request.user)
+            context['user_articles'] = user_articles
+        return context
 
 class ArticleDetail(DetailView):
     model = Article
